f6fa0172d5c18611145c19af9058f2843acd6f0a
Yerko Ortiz <yerko.ortizmora@gmail.com>
2024-08-15 14:19:24 -0400

    lecture 01 and lecture 02

---
+++ clase02/snippets/IntToBin/IntToBin.java
@@ -0,0 +1,19 @@
+public class IntToBin {
+    public static String intToBin(int n) {
+        String bin = "";
+        while(n != 0) {
+            bin = (n%2) + bin;
+            n /= 2;
+        }
+        return bin;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToBin(25));
+        System.out.println(intToBin(2505));
+        System.out.println(intToBin(8));
+        for(int i=0; i < 10000000; i++) {
+            System.out.println(intToBin(i));
+        }
+    }
+}
