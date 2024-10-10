1436144d9c9af58accc158ed783cc8fc858d9d5a
yerkortiz <yerko.ortiz@sumup.com>
2024-10-10 09:22:23 -0300

    sort linked list

--- clase09/Sorting/Sorting.java
+++ clase09/Sorting/Sorting.java
@@ -1,10 +1,12 @@
 package Sorting;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Collections;
 
 class Sorting {
-    static void ascendentSort(Integer[] A) {
+    // integer es distinto a int
+    static void ascendentSort(int[] A) {
         System.out.printf("Array a: ");
         for(int a : A) {
             System.out.printf(" %d", a);
@@ -12,6 +14,7 @@ class Sorting {
         System.out.printf("\n");
     
         Arrays.sort(A);
+
         System.out.printf("Sorted Array a: ");
         for(int a : A) {
             System.out.printf(" %d", a);
@@ -34,7 +37,7 @@ class Sorting {
         System.out.printf("\n");
     }
 
-    static void sortCollection(ArrayList<Integer> A) {
+    static void sortCollection(LinkedList<Integer> A) {
         System.out.printf("Array a: ");
         for(int a : A) {
             System.out.printf(" %d", a);
@@ -50,10 +53,14 @@ class Sorting {
     }
 
     public static void main(String[] args) {
-        Integer[] A = new Integer[]{6,1,23,21,52};
-        ascendentSort(A);
-        descendentSort(A);
-        ArrayList<Integer> Al = new ArrayList<Integer>(Arrays.asList(6,1,23,21,52));
-
+        // int[] A = new int[]{6,1,23,21,52};
+        // ascendentSort(A);
+        // descendentSort(A);
+        // Integer[] A = new Integer[]{6,1,23,21,52};
+        // ascendentSort(A);
+        // descendentSort(A);
+        // ArrayList<Integer> Al = new ArrayList<Integer>(Arrays.asList(6,1,23,21,52));
+        LinkedList<Integer> ll = new LinkedList<Integer>(Arrays.asList(6,1,23,21,52));
+        sortCollection(ll);
     }
 }
